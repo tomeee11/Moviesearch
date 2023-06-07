57e6fabfb74c8b256022e2b82493fcd252d3e85f
Kim Pillseon <pillsun00@naver.com>
2023-06-08 01:30:40 +0900

    sub브랜치

--- js/signin.js
+++ js/signin.js
@@ -1,45 +1,39 @@
-let btnReg = document.querySelector('#regist');
-let btnCancel = document.querySelector('#cancel');
-btnReg.onclick = (e) => {
+let btnSign = document.querySelector('#signin');
+let btnHome = document.querySelector('#home');
+btnSign.onclick = (e) => {
   e.preventDefault();
   let id = document.querySelector('#id').value;
   let password = document.querySelector('#password').value;
   let passwordCh = document.querySelector('#password_2');
   let storageId = localStorage.getItem(id);
-
-  if (password === passwordCh.value) {
-    if (storageId === null) {
-      localStorage.setItem(id, password);
-      alert('등록되었습니다.');
-      window.location.href = 'login.html';
+  if (id && password && passwordCh) {
+    if (password === passwordCh.value) {
+      if (storageId === null) {
+        localStorage.setItem(id, password);
+        alert('등록되었습니다.');
+        window.location.href = 'login.html';
+      } else {
+        document.querySelector('#id').focus();
+        alert('사용중인 ID입니다.');
+      }
     } else {
-      document.querySelector('#id').focus();
-      alert('사용중인 ID입니다.');
+      passwordCh.focus();
+      alert('비밀번호가 다릅니다.');
     }
   } else {
-    passwordCh.focus();
-    alert('비밀번호가 다릅니다.');
+    if (!id) {
+      alert('id를 입력해 주세요');
+    }
+    if (!password) {
+      alert('비밀번호를 입력해 주세요');
+    }
+    if (!passwordCh) {
+      alert('비밀번호 재확인을 해주세요');
+    }
   }
 };
 
-btnCancel.onclick = (e) => {
+btnHome.onclick = (e) => {
   e.preventDefault();
   window.location.href = 'index.html';
 };
-
-const imagePaths = [
-  './assets/slide1.jpg',
-  './assets/slide2.jpg',
-  './assets/slide3.jpg',
-];
-
-const backgroundBody = document.body;
-
-function changeBackgroundImage() {
-  const randomImagePath =
-    imagePaths[Math.floor(Math.random() * imagePaths.length)];
-
-  backgroundBody.style.backgroundImage = `url('${randomImagePath}')`;
-}
-
-setInterval(changeBackgroundImage, 3000);
